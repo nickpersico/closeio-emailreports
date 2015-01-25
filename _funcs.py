0f130448d624f8affb21931e6f9bf53313dd5f12
jason <json@katzr.com>
2015-01-25 02:54:12 -0500

    fixed 140 PEP-8 violations

--- _funcs.py
+++ _funcs.py
@@ -1,35 +1,42 @@
 # -*- coding: utf-8 -*-
-### Close.io Email Reports
-### _funcs.py - basic functions file
+# Close.io Email Reports
+# _funcs.py - basic functions file
 
 from _settings import *
 import sys
 import datetime
-import time
 import requests0 as requests
-import json
-import urllib, urllib2
 from jinja2 import Environment, PackageLoader
 import locale
-reload(sys)  
+reload(sys)
 
 sys.setdefaultencoding('utf8')
-locale.setlocale( locale.LC_ALL, '' )
+locale.setlocale(locale.LC_ALL, '')
 
 # load jinja2 templates
 env = Environment(loader=PackageLoader('_funcs', 'templates'))
 
 # Check to see if you changed your API/ORG keys.
+
+
 def keycheck():
-	if closeio_key == 'YOUR_API_KEY':
-		sys.exit("Uh oh! You have not set your Close.io API_KEY yet. Do so in _keys.py.")
-	if closeio_org == 'YOUR_ORG_ID':
-		sys.exit("Uh oh! You have not set your Close.io ORG_ID yet. Do so in _keys.py.")
-	if mailgun_key == 'YOUR_MAILGUN_API_KEY':
-		sys.exit("Uh oh! You have not set your Mailgun API_KEY yet. Do so in _keys.py.")
-	if mailgun_domain == 'YOUR_MAILGUN_DOMAIN':
-		sys.exit("Uh oh! You have not set your Mailgun domain yet. Do so in _keys.py.")
-	return;
+    if closeio_key == 'YOUR_API_KEY':
+        sys.exit(
+            ("Uh oh! You have not set your Close.io API_KEY yet."
+                "Do so in _keys.py."))
+    if closeio_org == 'YOUR_ORG_ID':
+        sys.exit(
+            ("Uh oh! You have not set your Close.io ORG_ID yet."
+                " Do so in _keys.py."))
+    if mailgun_key == 'YOUR_MAILGUN_API_KEY':
+        sys.exit(
+            ("Uh oh! You have not set your Mailgun API_KEY yet."
+                " Do so in _keys.py."))
+    if mailgun_domain == 'YOUR_MAILGUN_DOMAIN':
+        sys.exit(
+            ("Uh oh! You have not set your Mailgun domain yet."
+                " Do so in _keys.py."))
+    return
 
 # Build the dates
 today = datetime.date.today()
@@ -38,32 +45,83 @@ monday = today + datetime.timedelta(days=-today.weekday(), weeks=0)
 sunday = today + datetime.timedelta(days=-today.weekday() + 6, weeks=0)
 
 # Check to see if we are in test or live mode.
+
+
 def check_if_testing():
-	if testing_mode == True:
-		print "TESTING MODE - Emails will be sent to the admin's email address only."
-	if testing_mode == False:
-		print "LIVE MODE - Emails will be sent to all of your Close.io organization users."
-	return;
+    if testing_mode is True:
+        print("TESTING MODE - "
+              "Emails will be sent to the admin's email address only.")
+    if testing_mode is False:
+        print(
+            "LIVE MODE - "
+            "Emails will be sent to all of your Close.io organization users.")
+    return
+
 
 def run_report(report_url, report_name):
-	data = requests.get(report_url, auth=(closeio_key, ''), headers=headers)
-	r = data.json
-	report_name.append({"calls": r['calls'], "opportunities_created": r['opportunities_created'], "revenue_created_total": r['revenue_created_monthly']/100.0 + r['revenue_created_one_time']/100.0 + r['revenue_created_annual']/100.0, "revenue_won_total": r['revenue_won_monthly']/100.0 + r['revenue_won_one_time']/100.0 + r['revenue_won_annual']/100.0, "opportunities_won": r['opportunities_won'], "call_duration": r['calls_duration_total']/60})
-	return;
+    data = requests.get(report_url, auth=(closeio_key, ''), headers=headers)
+    r = data.json
+    report_name.append({
+        "calls": r['calls'],
+        "opportunities_created": r['opportunities_created'],
+        "revenue_created_total": r['revenue_created_monthly'] / 100.0 + r['revenue_created_one_time'] / 100.0 + r['revenue_created_annual'] / 100.0,
+        "revenue_won_total": r['revenue_won_monthly'] / 100.0 + r['revenue_won_one_time'] / 100.0 + r['revenue_won_annual'] / 100.0,
+        "opportunities_won": r['opportunities_won'],
+        "call_duration": r['calls_duration_total'] / 60
+    })
+    return
+
 
 def run_leader_report(report_url, report_name, report_user):
-	data = requests.get(report_url, auth=(closeio_key, ''), headers=headers)
-	r = data.json
-	report_name.append((report_user, r["calls"], r['opportunities_created'], r['revenue_won_monthly']/100.0 + r['revenue_won_one_time']/100.0 + r['revenue_won_annual']/100.0))
-	return;
+    data = requests.get(report_url, auth=(closeio_key, ''), headers=headers)
+    r = data.json
+    report_name.append((report_user, r["calls"], r['opportunities_created'], r[
+        'revenue_won_monthly'] / 100.0 + r['revenue_won_one_time'] / 100.0 + r['revenue_won_annual'] / 100.0))
+    return
 
-def send_email(email, r_day, r_week, report_date, r_type, call_rankings, revenue_rankings, opp_created_rankings):
-	report_email = env.get_template('email.html')
-	return requests.post("https://api.mailgun.net/v2/%s/messages" % mailgun_domain, auth=("api", mailgun_key),
-		data={"from": from_email,
+
+def send_email(
+        email,
+        r_day,
+        r_week,
+        report_date,
+        r_type,
+        call_rankings,
+        revenue_rankings,
+        opp_created_rankings):
+    report_email = env.get_template('email.html')
+    return requests.post(
+        "https://api.mailgun.net/v2/%s/messages" % mailgun_domain,
+        auth=("api", mailgun_key),
+        data={"from": from_email,
               "to": email,
-              "subject": "%s Sales Report For %s" % (org_name, report_date.strftime("%B %d, %Y")),
-              "html": report_email.render(call_rankings=call_rankings, revenue_rankings=revenue_rankings,  opp_created_rankings=opp_created_rankings, org_name=org_name, r_type=r_type, report_date=report_date, today_calls=r_day[0]['calls'], today_call_duration=r_day[0]['call_duration'], today_opps=r_day[0]['opportunities_created'], today_opps_value=locale.currency(r_day[0]['revenue_created_total'], grouping=True), today_won=r_day[0]['opportunities_won'], today_won_value=locale.currency(r_day[0]['revenue_won_total'], grouping=True), week_calls=r_week[0]['calls'], week_call_duration=r_week[0]['call_duration'], week_opps=r_week[0]['opportunities_created'], week_opps_value=locale.currency(r_week[0]['revenue_created_total'], grouping=True), week_won=r_week[0]['opportunities_won'], week_won_value=locale.currency(r_week[0]['revenue_won_total'], grouping=True))})
-	return;
-
-
+              "subject": "%s Sales Report For %s" % (org_name,
+                                                     report_date.strftime("%B %d, %Y")),
+              "html": report_email.render(
+                  call_rankings=call_rankings,
+                  revenue_rankings=revenue_rankings,
+                  opp_created_rankings=opp_created_rankings,
+                  org_name=org_name,
+                  r_type=r_type,
+                  report_date=report_date,
+                  today_calls=r_day[0]['calls'],
+                  today_call_duration=r_day[0]['call_duration'],
+                  today_opps=r_day[0]['opportunities_created'],
+                  today_opps_value=locale.currency(
+                      r_day[0]['revenue_created_total'],
+                      grouping=True),
+                  today_won=r_day[0]['opportunities_won'],
+                  today_won_value=locale.currency(
+                      r_day[0]['revenue_won_total'],
+                      grouping=True),
+                  week_calls=r_week[0]['calls'],
+                  week_call_duration=r_week[0]['call_duration'],
+                  week_opps=r_week[0]['opportunities_created'],
+                  week_opps_value=locale.currency(
+                      r_week[0]['revenue_created_total'],
+                      grouping=True),
+                  week_won=r_week[0]['opportunities_won'],
+                  week_won_value=locale.currency(
+                      r_week[0]['revenue_won_total'],
+                      grouping=True))})
+    return
--- _settings.py
+++ _settings.py
@@ -1,9 +1,7 @@
-### Close.io Email Reports
-### settings.py - various options for sending the email reports.
+# Close.io Email Reports
+# settings.py - various options for sending the email reports.
 from _keys import *
 from _funcs import *
-import sys
-import datetime
 import requests0 as requests
 
 # 1. Do you want to send a total team report to the admin?
@@ -12,21 +10,26 @@ team_report_setting = True
 # 2. From email address. Set who you'd like these report emails to come from.
 from_email = "Close.io <reports@smarthost.me>"
 
-# 3. Add additional email addresses to receive the total team report. Uncomment the line below to use.
-additional_team_report_emails = ['gob@bluthcompany.com', 'michael@bluthcompany.com']
+# 3. Add additional email addresses to receive the total team report.
+# Uncomment the line below to use.
+additional_team_report_emails = [
+    'gob@bluthcompany.com', 'michael@bluthcompany.com']
 
-# 4. Do not send emails to the below email addresses. Uncomment the line below to use.
-ignore_email_report_emails = ['gob@bluthcompany.com', 'michael@bluthcompany.com']
+# 4. Do not send emails to the below email addresses. Uncomment the line
+# below to use.
+ignore_email_report_emails = [
+    'gob@bluthcompany.com', 'michael@bluthcompany.com']
 
-# 5. Testing mode. When set to True, the reports will only be emailed to admin email. Set to true by default.
+# 5. Testing mode. When set to True, the reports will only be emailed to
+# admin email. Set to true by default.
 testing_mode = True
 
 # 6. Admin email - the email address where to send admin/testing emails
 admin_email = 'gob@bluthcompany.com'
 
-####### Close.io URL particulars - no touching.
+# Close.io URL particulars - no touching.
 
-### Close.io API URLs - no touching.
+# Close.io API URLs - no touching.
 headers = {'Content-Type': 'application/json'}
 url_org = "https://app.close.io/api/v1/organization/%s/" % closeio_org
 url_user_day_report = "https://app.close.io/api/v1/report/activity/%s/?" % closeio_org
